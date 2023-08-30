91bd875ada1de751209f4a67d836ac02f50d7428
RaviBey <112951109+RaviBey@users.noreply.github.com>
2023-08-30 21:14:54 +0400

    Create Bot.py

---
+++ Kelime_bot/Bot.py
@@ -0,0 +1,13 @@
+yeni_user = (
+"Xoş Gəldin Balam 😍",
+"Səni gözləməydən gözüm qaralıbe gəl çıxdaa",
+"Xoşsann Ürəyimm ❤️",
+)
+
+start = (
+"👋 Salam Xoş Gördük.\n✅ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam. Aktivləşdirmək üçün /chatbot Əmrindən isdifadə edin\nℹ Bot Barədə Tam Məlumat Almaq Üçün  **INFO** Buttonuna Toxunun",
+)
+
+info = (
+"☑ Mən XAOS Federasiyasının Rəsmi Söhbət Botuyam\n\nℹ Botumuz Haqda Qısa Məlumat\n⚡ 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋  Sıradan Söhbət Və Avto Cavablayıcı Botudur. Bu Botun Əsas Özəlliyi Qruplarda Mesaj Sayın Çoxaltmaq , Vaxdınızı Əyləncəli Keçirmək Dir. 𝕏𝔸𝕆𝕊 ℂℍ𝔸𝕋 𝔹𝕆𝕋 u qruplarınlza Alaraq Vaxdınızı Əyləncəli Və Səmərəli Keçirə Bilərsiz. Aktivləşdirmək üçün /chatbot Əmrindən isdifadə edin",
+)
